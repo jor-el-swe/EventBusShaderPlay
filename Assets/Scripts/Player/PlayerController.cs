27fc7685271e34fbba1f78af4dd0f73ce61e2729
Joel Ring <joel.ring@gmail.com>
2021-01-25 18:09:58 +0100

    adds some nice changes

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * (playerSpeed * Time.deltaTime);
+        
+    }
+}
