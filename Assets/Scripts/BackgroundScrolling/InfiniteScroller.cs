7d3836ae938f57a1e953ce35df50c6ca58811886
Joel Ring <joel.ring@gmail.com>
2021-01-26 13:50:58 +0100

    adds background plane

--- Assets/Scripts/BackgroundScrolling/InfiniteScroller.cs
+++ Assets/Scripts/BackgroundScrolling/InfiniteScroller.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class InfiniteScroller : MonoBehaviour{
-    private Transform thisTransform;
-    private Transform playerTransform;
-    private float startPositionZ;
+namespace BackgroundScrolling{
+    public class InfiniteScroller : MonoBehaviour{
+        private Transform thisTransform;
+        private Transform playerTransform;
+        private float startPositionZ;
     
-    // Start is called before the first frame update
-    void Start(){
-        thisTransform = this.gameObject.transform;
-        playerTransform = FindObjectOfType<PlayerController>().transform;
-        startPositionZ = playerTransform.position.z;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (playerTransform.position.z > thisTransform.position.z){
-            thisTransform.position = 
-                new Vector3(thisTransform.position.x, thisTransform.position.y, thisTransform.position.z + (playerTransform.position.z-startPositionZ));
+        // Start is called before the first frame update
+        void Start(){
+            thisTransform = transform;
+            playerTransform = FindObjectOfType<PlayerController>().transform;
             startPositionZ = playerTransform.position.z;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (playerTransform.position.z > thisTransform.position.z){
+                thisTransform.position = 
+                    new Vector3(thisTransform.position.x, thisTransform.position.y, thisTransform.position.z + (playerTransform.position.z-startPositionZ));
+                startPositionZ = playerTransform.position.z;
+            }
         }
     }
 }
---
+++ Assets/Scripts/BackgroundScrolling/MoveBackground.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 offset;
+    private Transform playerTransform;
+    void Start(){
+        playerTransform = FindObjectOfType<PlayerController>().transform;
+        offset = playerTransform.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        transform.position = playerTransform.position - offset;
+    }
+}
