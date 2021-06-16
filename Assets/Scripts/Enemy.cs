481ca9148b2d61394795806a5d5995c024a4b25b
Joel Ring <joel.ring@gmail.com>
2021-06-16 18:39:39 +0200

    adds player health message to enemy hit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+﻿using EventBrokerFolder;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        EventBroker.Instance().SendMessage(new PlayerHealthMessage(666));
+    }
+}
