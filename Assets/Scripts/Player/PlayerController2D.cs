a716d31d73ac31b0c4395b60b67bc2b23f187043
Joel Ring <joel.ring@gmail.com>
2021-06-16 19:01:12 +0200

    adds 2d scene

---
+++ Assets/Scripts/Player/PlayerController2D.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerController2D: MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal")*playerSpeed*Time.deltaTime, Input.GetAxis("Vertical") * playerSpeed*Time.deltaTime, 0.0f);
+    }
+}
